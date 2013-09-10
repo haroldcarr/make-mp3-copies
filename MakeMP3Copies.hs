7b2256153cdc8d951c7be29c36086dca23cf6325
Harold Carr <harold.carr@gmail.com>
2013-09-10 15:58:02 -0700

    first version before refactoring

---
+++ MakeMP3Copies.hs
@@ -0,0 +1,123 @@
+
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
+
+import           Control.Applicative
+import           Control.Exception (bracket, handle, SomeException)
+import           Control.Monad
+import           Control.Monad.IO.Class
+import           Data.Maybe (fromJust)
+import qualified Data.Text as T
+import           Shelly
+import           System.Directory
+import           System.FilePath
+import           System.IO (IOMode(..), hClose, hFileSize, openFile)
+default (T.Text)
+
+
+startingPath = "."
+copyRoot = "/tmp/JUNK/"
+
+
+main = shelly $ verbosely $ do
+    processDir startingPath
+
+
+processDir :: Shelly.FilePath -> Sh ()
+processDir path = do
+    contents <- ls path
+    forM_ contents $ \fp -> do
+            isDir <- test_d fp
+            if isDir
+                then do { maybeCreateDir fp; processDir fp}
+                else processFile fp $ takeExtension $ fpToString fp
+
+
+maybeCreateDir :: Shelly.FilePath -> Sh ()
+maybeCreateDir fp = do
+    let cp = mkCpFilePath fp
+    dirExists <- test_d cp
+    if dirExists
+        then                  say "DIR EXISTS"  cp
+        else do { mkdir_p cp; say "DIR CREATED" cp }
+
+
+processFile :: Shelly.FilePath -> String -> Sh ()
+processFile fp ".flac" = maybeConvert  fp
+processFile fp ".mp3"  = maybeCopy     fp
+processFile fp ".jpg"  = maybeCopy     fp
+processFile fp _       = say "IGNORED" fp
+
+
+maybeConvert fp = do
+    let cp = mkCpFilePath (fromText (T.pack (replaceExtension (fpToString fp) ".mp3")))
+    fileExists <- test_f cp
+    if fileExists
+        then convertIf fp cp
+        else convert   fp cp
+
+
+convertIf :: Shelly.FilePath -> Shelly.FilePath -> Sh ()
+convertIf fp cp = do
+    fpSize <- liftIO . getFileSize         $ fpToString fp
+    fpTime <- liftIO . getModificationTime $ fpToString fp
+    cpSize <- liftIO . getFileSize         $ fpToString cp
+    cpTime <- liftIO . getModificationTime $ fpToString cp
+    if fpTime > cpTime || (fromJust fpSize) /= (fromJust cpSize)
+        then convert fp cp
+        else say "FILE EXISTS" cp
+
+
+convert fp copy = do
+    flacToMp3 (toTextIgnore fp) (toTextIgnore copy)
+    say "FILE CONVERTED" copy
+  where
+    flacToMp3 from to = run_ "ffmpeg" ["-i", from, "-qscale:a", "0", to]
+    -- flacToMp3 from to = liftIO $ putStrLn $ "ffmpeg" ++ (show ["-i", "-n", from, "-qscale:a", "0", to])
+
+
+maybeCopy fp = do
+    let cp = mkCpFilePath fp
+    fileExists <- test_f cp
+    if fileExists
+        then copyIf fp cp
+        else copy   fp cp
+
+
+copyIf :: Shelly.FilePath -> Shelly.FilePath -> Sh ()
+copyIf fp cp = do
+    fpSize <- liftIO . getFileSize         $ fpToString fp
+    fpTime <- liftIO . getModificationTime $ fpToString fp
+    cpSize <- liftIO . getFileSize         $ fpToString cp
+    cpTime <- liftIO . getModificationTime $ fpToString cp
+    if fpTime > cpTime || (fromJust fpSize) /= (fromJust cpSize)
+        then copy fp cp
+        else say "FILE EXISTS" cp
+
+
+copy fp copyFile = do
+    cp fp copyFile
+    say "FILE COPIED" copyFile
+
+
+mkCpFilePath :: Shelly.FilePath -> Shelly.FilePath
+mkCpFilePath path =
+    (fpToString copyRoot) Shelly.</> (fpToString path)
+
+fpToString :: Shelly.FilePath -> String
+fpToString fp = T.unpack $ toTextIgnore fp
+
+
+say msg fp =
+    liftIO $ putStrLn $ show (fpToString fp) ++ " " ++ msg
+
+
+getFileSize :: System.FilePath.FilePath -> IO (Maybe Integer)
+getFileSize path = handle handler $
+    bracket (openFile path ReadMode) (hClose) (\h -> do
+        size <- hFileSize h
+        return $ Just size)
+  where
+    handler :: SomeException -> IO (Maybe Integer)
+    handler _ = return Nothing
--- README.md
+++
@@ -1,4 +0,0 @@
-make-mp3-copies
-===============
-
-make a parallel directory tree containing mp3 copies of flac, wav, mp3, etc., files in another tree
